64bb5061b1e0dae695282126ee4c989ff420e0f9
Onur Karaman <onur.o.karaman@daimler.com>
2019-08-06 10:44:08 +0200

    UserProfile: Model created.

---
+++ user_profile/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class UserProfile(models.Model):
+    """
+    Created automatically with a new user-model.
+    RuleModules can be linked to this model.
+    """
+    creation_date = models.DateTimeField(default=timezone.now)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
