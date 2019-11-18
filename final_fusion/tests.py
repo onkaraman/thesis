6501b1af80258ed5220fd5fe77a2e50155dffc6a
Onur Karaman <onur.o.karaman@daimler.com>
2019-11-18 12:57:42 +0100

    FF: Tests added.

--- final_fusion/tests.py
+++ final_fusion/tests.py
@@ -28,7 +28,7 @@ class TestsFFModel(TestCase):
         ff = FinalFusion.objects.first()
 
         # Import sample json
-        json_parsed = tq_v.delegate_to_parser("sample_import_files/unsorted/cars.json",
+        json_parsed = tq_v.delegate_to_parser("sample_import_files/unsorted/items_with_duplicates.json",
                                               "json", None)
         TQFile.objects.create(
             project=project,
@@ -63,9 +63,53 @@ class TestsFFModel(TestCase):
         :return:
         """
         project = Project.objects.first()
-        tq_table = TQFile.objects.first().get_as_table(project.user_profile)
+        ff = FinalFusion.objects.get(project=project)
+        tq = TQFile.objects.first()
 
         # Check 'first_name'
-        self.assertTrue(tq_table["cols"][1]["ef_added"])
+        self.assertTrue(ff.tq_column_is_added("first_name", tq))
         # Check 'last_name'
-        self.assertTrue(tq_table["cols"][2]["ef_added"])
+        self.assertTrue(ff.tq_column_is_added("last_name", tq))
+
+    def test_tq_column_not_added(self):
+        """
+        Check if unselected TQs show not as marked.
+        :return:
+        """
+        project = Project.objects.first()
+        ff = FinalFusion.objects.get(project=project)
+        tq = TQFile.objects.first()
+
+        # Check 'first_name'
+        self.assertFalse(ff.tq_column_is_added("car", tq))
+        # Check 'last_name'
+        self.assertFalse(ff.tq_column_is_added("no_column_name", tq))
+
+    def test_col_vars(self):
+        project = Project.objects.first()
+        ff = FinalFusion.objects.get(project=project)
+
+        col_vars = ff.get_col_vars()
+
+        # Only two vars should be present (only two cols selected in setup)
+        self.assertTrue(len(col_vars))
+        # Each col_var should have no more than 4 characters
+        for cv in col_vars:
+            self.assertTrue(len(cv) <= 4)
+
+    def test_count_duplicates(self):
+        project = Project.objects.first()
+        ff = FinalFusion.objects.get(project=project)
+
+        self.assertTrue(ff.count_duplicates(project.user_profile) > 0)
+
+    def test_remove_duplicates(self):
+        project = Project.objects.first()
+        ff = FinalFusion.objects.get(project=project)
+
+        table_w_dupes = ff_v.get_preview_table(project.user_profile)
+
+        dupe_count = len(table_w_dupes["out_rows"])
+        non_dupe_count = len(ff.remove_duplicates(table_w_dupes["out_rows"]))
+        self.assertTrue(dupe_count > non_dupe_count)
+
